d095ac867ce82c5782d22ded5a7ed5c733735eb8
Harminder Virk <virk.officials@gmail.com>
2015-02-01 15:56:20 +0530

    Added required comments

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,15 @@
   2. If your app does not have routes , simple export an empty object
       @example
         module.exports = {}
+
+  ----------------------------------------
+                EXAMPLE
+  ----------------------------------------
+  {
+    url: "/",
+    templateUrl: "templates/home.html",
+    controller: "homeController"
+  }
  */
 var routeMap;
 
